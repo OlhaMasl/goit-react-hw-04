e1018105eb259c489faf09b9444d4addbd6010ad
OlhaMasl <maslakok@gmail.com>
2024-07-20 13:47:14 +0300

    Init pj

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,20 @@
+import s from "./SearchBar.module.css"
+
+const SearchBar = () => {
+    return (
+        <header className={s.header}> 
+  <form className={s.serchForm}>
+    <input
+      type="text"
+      autoComplete="off"
+      autoFocus 
+      placeholder="Search images and photos"
+      className={s.serchImput}              
+    />
+    <button className={s.searchBtn} type="submit">Search</button>
+  </form>
+</header>
+
+    );
+};
+export default SearchBar;
